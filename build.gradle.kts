e7d90fade990e58d7fe366c2b7a3305c9efdcc80
Belal123-hub <131976726+Belal123-hub@users.noreply.github.com>
2024-06-20 01:10:54 +0700

    add serialization lib to main gardle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-
+    alias(libs.plugins.kotlin.serialization) apply false
 }
