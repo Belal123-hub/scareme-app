ffe5007ee46561f1cb46c4cf19685d9e37a3b615
Belal123-hub <131976726+Belal123-hub@users.noreply.github.com>
2024-06-20 02:35:55 +0700

    setting access token in data layer and getting the refreshToken

--- data/src/main/java/com/example/data/dataStore/DataStoreDataSourceImpl.kt
+++ data/src/main/java/com/example/data/dataStore/DataStoreDataSourceImpl.kt
@@ -5,8 +5,10 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.domain.dataStore.DataStoreDataSource
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "app_name")
@@ -16,18 +18,25 @@ class DataStoreDataSourceImpl(
 ) : DataStoreDataSource {
 
     private val dataStore = context.dataStore
-
-    override val isUserSignedIn = dataStore.data
-        .map { preferences -> preferences[IS_USER_SIGNED_IN] ?: false }
-
-
-    override suspend fun setIsUserSignedIn(isUserSignedIn: Boolean) {
+    override val accessToken = dataStore.data.map { preferences -> preferences[ACCESS_TOKEN]?.ifEmpty { null } }
+    override suspend fun setAccessToken(accessToken: String?) {
         dataStore.edit { preferences ->
-            preferences[IS_USER_SIGNED_IN] = isUserSignedIn
+            preferences[ACCESS_TOKEN] = accessToken.orEmpty()
         }
     }
 
+    override suspend fun getRefreshToken() = dataStore.data.first()[REFRESH_TOKEN]?.ifEmpty { null }
+
+    override suspend fun setRefreshToken(refreshToken: String?) {
+        dataStore.edit { preferences ->
+            preferences[REFRESH_TOKEN] = refreshToken.orEmpty()
+        }
+    }
+
+
+
     private companion object {
-        val IS_USER_SIGNED_IN = booleanPreferencesKey("is_user_signed_in")
+        val ACCESS_TOKEN = stringPreferencesKey("access_token")
+        val REFRESH_TOKEN = stringPreferencesKey("refresh_token")
     }
 }
--- domain/src/main/java/com/example/domain/dataStore/DataStoreDataSource.kt
+++ domain/src/main/java/com/example/domain/dataStore/DataStoreDataSource.kt
@@ -3,7 +3,7 @@ package com.example.domain.dataStore
 import kotlinx.coroutines.flow.Flow
 
 interface DataStoreDataSource {
-    val accessToken: Flow<String>
+    val accessToken: Flow<String?>
 suspend fun setAccessToken(accessToken: String?)
 suspend fun getRefreshToken():String?
 suspend fun setRefreshToken(refreshToken:String?)
